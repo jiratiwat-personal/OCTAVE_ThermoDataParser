20d44429afc9c52feb50d49087a744cec7b71aef
Jiratiwat Sinchai <jiratiwat_ku@hotmail.com>
2022-04-28 17:52:11 +0200

    update calculation and plotParsed

--- plotParsed.m
+++ plotParsed.m
@@ -1,23 +1,23 @@
 function [P,Q,R]= plotParsed(Cp,S,H,H_down,H_up,T,compound)
- 
- 
+
+
  name_compound=strrep(compound,'_','-');
- figure('Name',sprintf('Thermodynamic Plots',1)) 
+ figure('Name',sprintf('Thermodynamic Plots',1))
  subplot(1,3,1);
  P=plot(T,Cp,'linewidth',2,'color','blue');
  grid on
  xlabel('Temperature [K]');
  ylabel('Specific Heat [J/(mol*K)]');
  title(sprintf('Specific heat vs Temperature of %s ',name_compound));
- 
+
  subplot(1,3,2);
  Q=plot(T,S,'linewidth',2,'color','green');
  grid on
  xlabel('Temperature');
- ylabel('Entropy [j/mol]');
+ ylabel('Entropy [J/mol]');
  title(sprintf('Entropy vs Temperature of %s ',name_compound));
- 
- 
+
+
  subplot(1,3,3);
  R=plot(T,H,'linewidth',2,'color','black');
  grid on
@@ -25,14 +25,14 @@ function [P,Q,R]= plotParsed(Cp,S,H,H_down,H_up,T,compound)
  ylabel('Enthalpy [J/(mol*K)]');
  title(sprintf('Enthalpy vs Temperature of %s ',name_compound));
  hold on
- 
+
  plot(T,H_down,'linewidth',2,'color','blue');
  plot(T,H_up,'linewidth',2,'color','red');
- 
+
  h=legend("Original","Decrement","Increment");
- 
+
  hold off
- 
+
  %% ================creating folder and saving plots======================
  E=pwd;
  mkdir ('Plots');
@@ -40,7 +40,7 @@ function [P,Q,R]= plotParsed(Cp,S,H,H_down,H_up,T,compound)
  figure_name = sprintf('%s.jpg', compound);
  saveas(figure(1),figure_name);
  cd(E)
- 
- 
- 
+
+
+
 end
