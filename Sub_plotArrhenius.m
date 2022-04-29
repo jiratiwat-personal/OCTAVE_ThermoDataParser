9dd40681fe8ba6b93f6cd84230bad6d60d227ffe
jiratiwat <jiratiwat_ku@hotmail.com>
2022-04-29 02:41:38 +0200

    Parser from user data

--- Sub_plotArrhenius.m
+++ Sub_plotArrhenius.m
@@ -1,12 +1,18 @@
 #This is an easy calculation and plot for Arrhenius parameters.
+prompt=('Enter A N and E for reaction1: ');
+strReaction1=input(prompt,'s');
+reaction1=str2double(strsplit(strReaction1));
+prompt=('Enter A N and E for reaction2: ');
+strReaction2=input(prompt,'s');
+reaction2=str2double(strsplit(strReaction2));
 
 #Constant
 R=[8.31451e0];
 P=[1.0133e5];
 
 #Arrhenius paramters
-reaction1=[+1.284751E20, -1.026757E00, +1.785516E02];
-reaction2=[+1.848080E20, -1.074854E00, +1.787606E02];
+#reaction1=[+1.284751E20, -1.026757E00, +1.785516E02];
+#reaction2=[+1.848080E20, -1.074854E00, +1.787606E02];
 A1=reaction1(1)
 A2=reaction2(1)
 N1=reaction1(2)
@@ -38,7 +44,7 @@ answer=power(2,2)
 #Plotting value
 x1=k1ln;
 x2=k2ln;
-semilogx (T,x1,";reaction1;",T,x2,";reaction2;","linewidth",5);
+semilogx (T,x1,";reaction1;",T,x2,";reaction2;");
 legend ("location", "northeast");
 title ("Arrhenius plot","fontsize",15)
 xlabel("Temperature [K]","fontsize",15);
